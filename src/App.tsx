17b5ccb26c1da4312afc59d4173eb52eb86ff82c
Renan <renan.fachin@hotmail.com>
2022-10-12 23:54:06 -0300

    add header and form to page

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,51 @@ import './styles/global.css'
 import { Logo } from './logo'
 import { Heading } from './components/Heading'
 import { Text } from './components/Text'
+import { TextInput } from './components/TextInput'
+
+// Importação de ícones
+import { Envelope, Lock } from 'phosphor-react'
 
 export function App() {
   return (
-    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
-      <Logo />
+    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
+      
+      <header className="flex flex-col items-center">
+        <Logo />
 
-      <Heading size="lg">
-        Ignite Lab
-      </Heading>
+        <Heading size="lg" className="mt-4">
+          Ignite Lab
+        </Heading>
 
-      <Text size="lg" className="text-gray-400">
-        Faça login e comece a usar!
-      </Text>
+        <Text size="lg" className="text-gray-400 mt-1">
+          Faça login e comece a usar!
+        </Text>
+      </header>
+
+      <form className="flex flex-col gap-4 items-stretch w-full max-w-[400px] mt-10">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">Endereço de e-mail</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+
+            <TextInput.Input type= "email" id="email" placeholder="Digite seu e-mail"/>
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="password" className="flex flex-col gap-3">
+          <Text className="font-semibold">Sua senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+
+            <TextInput.Input type= "password" id="password" placeholder="********"/>
+          </TextInput.Root>
+        </label>
+      </form>
+
     </div>
   )
 }
