105c372ce55364c33ac538483fd8cc627351b54e
Renan <renan.fachin@hotmail.com>
2022-10-14 17:36:26 -0300

    changing to asynchronous function test response expectation

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -5,7 +5,7 @@ import { Meta, StoryObj } from '@storybook/react'
 import { SignIn } from './SignIn'
 
 // Imports de addons do storybook
-import { within, userEvent} from '@storybook/testing-library'
+import { within, userEvent, waitFor} from '@storybook/testing-library'
 import { expect } from '@storybook/jest'
 
 // Exportando a config. global do componente
@@ -20,7 +20,10 @@ export const Default: StoryObj = {
     // Criando os testes
     // Dentro do play vai ter os testes automatizados
     // canvasElement é o wideframe que mostrar o componente no storybook
-    play: ({canvasElement}) => {
+    
+    
+    // Será necessário dizer que a função é async em virtude da "demora" da atualização de estado
+    play: async ({canvasElement}) => {
         const canvas = within(canvasElement)
 
         // Simulando a digitação dos campos
@@ -33,7 +36,11 @@ export const Default: StoryObj = {
         
         // É possível dizer o que é esperado que aconteça após a realização do teste
         // Neste caso, como é uma simulação, espera que apareça um texto em tela
+        // Porém a troca de estado no react não é instantâneo. Demora algumas milisegundos para que aconteça
 
-        expect(canvas.getByText('Login realizado!')).toBeInTheDocument()
+        // Então, usamos o await e o waitFor para aguardar a atualização do estado
+        await waitFor(() => {
+            expect(canvas.getByText('Login realizado!')).toBeInTheDocument()
+        })
     }
 }
